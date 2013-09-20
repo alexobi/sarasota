f168d2cd01bd34ae92e5d29fb3d12e720b7faee1
Alexander Obi <alexebube@gmail.com>
2013-09-20 10:37:42 -0400

    Created a new repo for the conversion of sarasota static html template to a working wordpress theme. Added initial wordpress template files to the repo sarasota

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <title><?php wp_title(); ?></title>
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
+        <link href="css/sarasota.css" rel="stylesheet" type="text/css">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <? wp_head(); ?>
+    </head>
+    <body>
+        <div id="wrap">
+            <header id="header" class="row">
+                <div class="container">
+                <nav id="navigate">
+                    <a href="#" class="showMenu">Menu</a>
+                        <ul id="navlist" class="nav">
+                            <li class="active"><a href="index.html">Home</a></li>
+                            <li><a href="#">Fact</a>
+                            <li><a href="#">Action</a>
+                            <li><a href="blog.html">Blog</a>
+                            </li>
+                            </li>
+                        </ul>
+                </nav>
+                </div>
+                <div class="container">
+                    <h1 class="logohead container"><a href="index.html">Lorem Ipsum</a></h1>
+                    <p class="lang container">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus pharetra bibendum. Pellentesque dolor justo, suscipit nec turpis in, tempus aliquam nulla. Praesent pellentesque commodo sapien non iaculis.</p>
+                </div>
+            </header>
+                <section id="main-section" class="row">
+                     <div class="container">
