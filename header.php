0140849ae92e604775f35a0eae2e13ca340cbf95
Alexander Obi <alexebube@gmail.com>
2013-09-20 12:20:12 -0400

    Modified the header.php file to include the following changes 1.Added the wordpress markups for link and meta tags 2.Added the bloginfo and wp_title functions of the wordpress engine 3.Added wp_nav_menu function to display menu in wordpress

--- header.php
+++ header.php
@@ -1,30 +1,46 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?> >
     <head>
-        <title><?php wp_title(); ?></title>
-        <link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
-        <link href="css/sarasota.css" rel="stylesheet" type="text/css">
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <? wp_head(); ?>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        
+        <title><?php wp_title( '|', true, 'right' ); ?></title>
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
+       
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+        <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+        <!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<![endif]-->
+        <? wp_head(); ?>
     </head>
-    <body>
+    <body <?php body_class(); ?>>
         <div id="wrap">
             <header id="header" class="row">
                 <div class="container">
                 <nav id="navigate">
                     <a href="#" class="showMenu">Menu</a>
-                        <ul id="navlist" class="nav">
-                            <li class="active"><a href="index.html">Home</a></li>
-                            <li><a href="#">Fact</a>
-                            <li><a href="#">Action</a>
-                            <li><a href="blog.html">Blog</a>
-                            </li>
-                            </li>
-                        </ul>
+                         <?php wp_nav_menu( array(
+						'container'=>'',
+						'theme_location' =>'primary',
+						'menu' => 'Primary Main',
+						'menu_class'=>'nav',
+                                                'menu_id' => 'navlist'
+						)); ?>
                 </nav>
                 </div>
                 <div class="container">
-                    <h1 class="logohead container"><a href="index.html">Lorem Ipsum</a></h1>
+                    <h1 class="logohead container"><a href="<?php echo site_url(); ?>">Lorem Ipsum</a></h1>
                     <p class="lang container">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus pharetra bibendum. Pellentesque dolor justo, suscipit nec turpis in, tempus aliquam nulla. Praesent pellentesque commodo sapien non iaculis.</p>
                 </div>
             </header>
