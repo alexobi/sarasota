c2a95ca289734f9756afe8cc2af640f45e798215
Alexander Obi <alexebube@gmail.com>
2013-09-23 08:00:36 -0400

    Removed the sylesheet link tag from the header.php file

--- header.php
+++ header.php
@@ -15,7 +15,6 @@
         
         <title><?php wp_title( '|', true, 'right' ); ?></title>
         <link rel="profile" href="http://gmpg.org/xfn/11" />
-        <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
        
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
